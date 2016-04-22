e229fa6ac8dfeb95987e392b7f2ab83f910d0acb
Willian Serafini <willian@onehost.com.br>
2016-04-21 23:26:56 -0300

    atualizando

--- classes/regra.php
+++ classes/regra.php
@@ -19,13 +19,85 @@ class Regra {
         $this->tabelaCache = $this->_getTabelaCacheInicial();
     }
 
+    public function setNumeroHits() {
+        $this->numeroHits ++;
+    }
+
+    public function getNumeroHits() {
+        return $this->numeroHits;
+    }
+
+    public function setNumeroMiss() {
+        $this->numeroMiss ++;
+    }
+
+    public function getNumeroMiss() {
+        return $this->numeroMiss;
+    }
+
+    public function setTabelaCache($tabelaCache) {
+        $this->tabelaCache = $tabelaCache;
+    }
+
+    public function getTabelaCache() {
+        return $this->tabelaCache;
+    }
+
     public function gerar() {
         $this->mostrarTabela();
+
         $linhasArquivo = file($this->arquivo);
 
         foreach ($linhasArquivo as $linha) {
-            
+            if ($this->_existeValorNaMemoria($linha)) {
+                $this->setNumeroHits();
+            } else {
+                $this->setNumeroMiss();
+            }
+
+            $this->setEnderecoNaMemoria(trim($linha));
+            $this->mostrarTabela();
         }
+    }
+
+    private function _existeValorNaMemoria($linha) {
+        $memoriaCache = $this->getTabelaCache();
+        $idx = self::getIdxEndereco($linha);
+
+        if (!isset($memoriaCache[$idx])) {
+            return false;
+        }
+
+        $tag = self::getTagEndereco($linha);
+        if ($memoriaCache[$idx]['tag'] != $tag) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function setEnderecoNaMemoria($endereco) {
+        $memoriaCache = $this->getTabelaCache();
+        $idx = self::getIdxEndereco($endereco);
+        $tag = self::getTagEndereco($endereco);
+        $memoriaCache[$idx] = [
+            'v' => 1,
+            'tag' => $tag,
+            'data1' => 'mem(' . $endereco . ')',
+            'data2' => 'mem(' . $endereco . ')',
+            'data3' => 'mem(' . $endereco . ')',
+            'data4' => 'mem(' . $endereco . ')'
+        ];
+
+        $this->setTabelaCache($memoriaCache);
+    }
+
+    public static function getIdxEndereco($endereco) {
+        return substr(trim($endereco), -2, 1);
+    }
+
+    public static function getTagEndereco($endereco) {
+        return substr(trim($endereco), 0, 6);
     }
 
     private function _isPassoAPasso($arrayParametros) {
@@ -61,13 +133,13 @@ class Regra {
     }
 
     public function mostrarTabela() {
-        $mask = "|%3s|%1s|%6s|%13s|%13s|%13s|%13s|\n";
+        $mask = "|%3s|%1s|%7s|%17s|%17s|%17s|%17s|\n";
         printf($mask, 'Idx', 'V', 'Tag', 'data', 'data', 'data', 'data');
-        foreach ($this->tabelaCache as $linha => $valores) {
+        foreach ($this->getTabelaCache() as $linha => $valores) {
             printf($mask, $linha, $valores['v'], $valores['tag'], $valores['data1'], $valores['data2'], $valores['data3'], $valores['data4']);
         }
 
-        echo "\n HITS: " . $this->numeroHits . "     MISSES: " . $this->numeroMiss . "\n";
+        echo "\n HITS: " . $this->getNumeroHits() . "     MISSES: " . $this->getNumeroMiss() . "\n";
         echo "\n Pressione enter para continuar... \n";
         fgetc(STDIN);
     }
