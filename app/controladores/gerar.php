3020d6c6d659a65f4adcd7db00b885f53813b7cb
Willian Serafini <willian@onehost.com.br>
2016-04-30 22:25:54 -0300

    adicionei funcao que remove parametros desnecessarios da linha de comando

--- app/controladores/gerar.php
+++ app/controladores/gerar.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('app/classes/regra.php');
+
 /**
  * Description of Gerar
  *
@@ -10,6 +11,7 @@ class Gerar {
 
     public function gerarTabelas($arrayParametros) {
         try {
+            $this->_removeParametrosDesnecessarios($arrayParametros);
             $this->_validaParametros($arrayParametros);
             $objRegra = new Regra($arrayParametros);
             $objRegra->gerar();
@@ -23,22 +25,29 @@ class Gerar {
         if ($numeroParametros == 0) {  //tem q ter pelo menos o parametro do .txt
             throw new Exception("É necessário pelo menos o arquivo .txt!\n");
         }
-        
+
         if ($numeroParametros > 2) {
             throw new Exception("Só é valido passar 2 parametros apenas!\n");
         }
-        
+
         $temArquivoTxt = false;
         foreach ($arrayParametros as $parametro) {
-            if(preg_match('/.txt$/', $parametro)) {
+            if (preg_match('/.txt$/', $parametro)) {
                 $temArquivoTxt = true;
                 break;
             }
         }
-        
-        if(!$temArquivoTxt) {
+
+        if (!$temArquivoTxt) {
             throw new Exception("É necessário o arquivo no formato .txt!\n");
         }
+    }
+
+    private function _removeParametrosDesnecessarios(&$arrayParametros) {
+        unset($arrayParametros[0]); // o 1 parametro é o main.php, entao removo do array
+        unset($arrayParametros[1]); // o 2 parametro é o ./chachesim, entao removo tbm
+        
+        $arrayParametros = array_values($arrayParametros); // indexa o array na ordem correta
     }
 
 }
