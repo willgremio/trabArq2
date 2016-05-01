9bd4cb39adce4f855fae041b54e79e7a8a3aec24
Willian Serafini <willian@onehost.com.br>
2016-04-30 22:26:25 -0300

    removi os unset e coloquei no controlador

--- app/main.php
+++ app/main.php
@@ -2,11 +2,5 @@
 
 require_once('app/controladores/gerar.php');
 
-// $argv é o array dos parametros passados na linha de comando
-unset($argv[0]); // o 1 parametro é o main.php, entao removo do array
-unset($argv[1]); // o 2 parametro é o ./chachesim, entao removo tbm
-
-$arrayParametros = array_values($argv); // indexa o array na ordem correta
-
 $objGerar = new Gerar();
-$objGerar->gerarTabelas($arrayParametros);
+$objGerar->gerarTabelas($argv); // $argv é o array dos parametros passados na linha de comando
